4d66d2a5ebb9f06399a408e5aa355adbaff259be
Anx450z <anx450z@gmail.com>
2022-07-14 15:06:03 +0530

    use call back function anf function type

--- app.ts
+++ app.ts
@@ -6,10 +6,11 @@ function printResult(num: number): void { // void return type as nothing is gett
   console.log('Result: ' + num);
 }
 
-// function printResult(num: number): undefined { // undefined return type as nothing is getting returned by default
-//   console.log('Result: ' + num);
-//   return;
-// }
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void){ // cb takes a function
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 let combineValues: (a: number, b:number) => number; //function types
@@ -19,3 +20,8 @@ combineValues = add; // works
 
 
 console.log(combineValues(8,8));
+
+addAndHandle(10, 20, (result) => { // we don't need to assign type of result here, it is done above
+  console.log(result);// result will be of number type
+  return result; // we can return value here, but will not be used inside addAndHandle function as return type is void
+})
